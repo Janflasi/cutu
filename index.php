f3cc440254b5e7d3f58e1b7c149c0562587682af
juandieg <corporalmovimientos123@gmail.com>
2024-07-29 11:28:07 -0500

    si señor

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section>
+
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+<div class="caja"></div>
+</section>
+    
+</body>
+</html>
